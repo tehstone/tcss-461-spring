c1118aec1249171dc589bba69f9c0a2d6eddb679
Andrew Leach <leachad@uw.edu>
2015-04-08 15:24:52 -0700

    Refactoring: Used previously extracted methods to reduce redundancy of code in CheckInventory method. Implementation: Used the getItemAmount and the readily available public static array of items in Inventory class to display levels of inventory to user. Eliminated the need for repetitive space-consuming println statements.

--- VMS-v0.0/src/TacomaVendingMachine.java
+++ VMS-v0.0/src/TacomaVendingMachine.java
@@ -65,7 +65,6 @@ class TacomaVendingMachine {
 		System.out.println("");
 
 		try {
-
 			for (int i = 0; i < data.myItems.length; i++) {
 				data.setItemAmount(data.myItems[i],
 						addInventoryItem(data.myItems[i]));
